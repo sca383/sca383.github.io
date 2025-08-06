20ca6dba5d0d90d0486d28bd4198cac8f61c2ef2
sca383 <sca383@sfu.ca>
2025-08-05 23:42:50 -0700

    updates

--- Shana_Chan-Site/src/firebase/config.js
+++ Shana_Chan-Site/src/firebase/config.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7b2oACmifQl03-_a6zj0mig1C2IcMitw",
-  authDomain: "shana-chan-site.firebaseapp.com",
-  projectId: "shana-chan-site",
-  storageBucket: "shana-chan-site.firebasestorage.app",
-  messagingSenderId: "928261399891",
-  appId: "1:928261399891:web:cb13ff277a37d3c738ada6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
